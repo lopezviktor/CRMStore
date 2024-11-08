e397847e966a6c31cebb2220a7bf3ac9b3d224ac
Juanma <jmconsentino@gmail.com>
2024-11-08 13:03:31 +0100

    Prueba Sync

--- app/src/main/java/com/example/crmstore/ui/screens/clientes/PantallaAddCliente.kt
+++ app/src/main/java/com/example/crmstore/ui/screens/clientes/PantallaAddCliente.kt
@@ -1,9 +1,14 @@
 package com.example.crmstore.ui.screens.clientes
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun PantallaAddCliente(navHostController: NavHostController){
+
+    Text(
+        text = "AÑADIR NUEVO CLIENTE"
+    )
 
 }
