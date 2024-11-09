fdc84217d6e724e256560eeeddded9dfb262fcda
Victor <victorlopezlopez93@gmail.com>
2024-11-09 19:40:00 +0100

    feat: pantalla de registro

--- app/src/main/java/com/example/crmstore/ui/screens/PantallaRegistro.kt
+++ app/src/main/java/com/example/crmstore/ui/screens/PantallaRegistro.kt
@@ -1,13 +1,143 @@
 package com.example.crmstore.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.crmstore.componentes.BotonEstandar
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun PantallaRegistro(navHostController: NavHostController){
+fun PantallaRegistro(
+    navHostController: NavHostController
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var mensajeError by remember { mutableStateOf("") }
 
-    Text(
-        text = "REGISTROS NUEVOS POR MAIL"
-    )
+    val auth = FirebaseAuth.getInstance()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(Color(0xFF2196F3), Color(0xFF1F2859))
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Registro de Usuario",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Correo Electrónico") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Contraseña") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            OutlinedTextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                label = { Text("Confirmar Contraseña") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+
+            BotonEstandar(
+                texto = "Registrarse",
+                onClick = {
+                    if (email.isNotBlank() && password.isNotBlank() && confirmPassword.isNotBlank()) {
+                        if (password == confirmPassword) {
+                            registrarUsuario(email, password, auth) { resultado ->
+                                if (resultado == "Éxito") {
+                                    navHostController.navigate("pantallaLogin")
+                                } else {
+                                    mensajeError = resultado
+                                }
+                            }
+                        } else {
+                            mensajeError = "Las contraseñas no coinciden."
+                        }
+                    } else {
+                        mensajeError = "Por favor, complete todos los campos."
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            if (mensajeError.isNotEmpty()) {
+                Text(
+                    text = mensajeError,
+                    color = Color.Red,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+fun registrarUsuario(email: String, password: String, auth: FirebaseAuth, onResult: (String) -> Unit) {
+    auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onResult("Éxito")
+            } else {
+                onResult(task.exception?.message ?: "Error al registrar")
+            }
+        }
 }
