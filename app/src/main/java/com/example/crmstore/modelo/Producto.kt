d9d0cb4650b5125da0b59884a9cc92791e3714d5
Victor <victorlopezlopez93@gmail.com>
2024-12-10 09:05:47 +0100

    Conseguido actualizar stock en cada venta

--- app/src/main/java/com/example/crmstore/modelo/Producto.kt
+++ app/src/main/java/com/example/crmstore/modelo/Producto.kt
@@ -1,13 +1,10 @@
 package com.example.crmstore.modelo
 
 data class Producto(
-    var id: String,
+    var id: String = "",
     val nombre: String = "",
     val descripcion: String = "",
     val precio: Double = 0.0,
     val stock: Int = 0,
     val categoria: String? = null
-){
-    constructor() : this(null.toString(), "", "", 0.0, 0, null)
-
-}
+)
